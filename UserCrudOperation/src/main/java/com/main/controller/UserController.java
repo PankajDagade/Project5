59a8df3bea33fcdb5fb7871c67fc0af130818608
PankajDagade <pankajdagade1998@gmail.com>
2024-07-04 16:39:38 +0530

    Push By Pankaj

--- UserCrudOperation/src/main/java/com/main/controller/UserController.java
+++ UserCrudOperation/src/main/java/com/main/controller/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +17,20 @@ public class UserController {
 	
 	@Autowired UserService userService;
 	
+	@PostMapping("/saveData")
 	public ResponseEntity<User> post(@RequestBody User user)
 	{
 		User u = userService.postData(user);
 		
 		return new ResponseEntity<User>(u,HttpStatus.CREATED);
+	}
+	
+	@PatchMapping("/s")
+	public ResponseEntity<String> s()
+	{
+		System.out.println("Pankaj");
+		
+		return "Pankaj";
 	}
 
 }
